2d3d4a803b20843daafc4d132b15eb6d56aa71c3
ishagupta10-git <ishag7429@gmail.com>
2025-07-10 23:18:38 +0530

    Update apiPaths.js

--- Frontend/src/utils/apiPaths.js
+++ Frontend/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+export const BASE_URL = "https://priorix.onrender.com";
 
 // utils/apiPaths.js
 export const API_PATHS = {
