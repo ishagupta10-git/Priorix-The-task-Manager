e45db0bf2df89b0f4194517277d940bf75298bd7
isha <ishag7429@gmail.com>
2025-07-10 22:20:53 +0530

    Changing Mongo_uri

--- Backendd/config/db.js
+++ Backendd/config/db.js
@@ -1,12 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {});
-    console.log("MongoDB connected");
+    await mongoose.connect(process.env.MONGO_URI, {
+    });
+    console.log("✅ Connected to MongoDB Atlas");
   } catch (err) {
-    console.error("Error connecting to MongoDB", err);
-    process.exit(1);
+    console.error("❌ DB connection error:", err.message);
+    process.exit(1); //  exit to avoid running unstable app
   }
 };
 
